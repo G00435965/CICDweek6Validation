1949ef67b5d73fb38f518fefc88f1b520421e732
G00435965 <g00435965@atu.ie>
2025-12-31 01:06:45 +0000

    added to passenger

--- src/main/java/ie/atu/cicdweek5/errorHandling/DuplicateException.java
+++ src/main/java/ie/atu/cicdweek5/errorHandling/DuplicateException.java
@@ -1,7 +1,15 @@
 package ie.atu.cicdweek5.errorHandling;
 
-public class DuplicateException extends RuntimeException {
-    public DuplicateException(String message) {
-        super(message);
+public class DuplicateException extends RuntimeException
+{
+    private String message;
+    private String field;
+    public DuplicateException(String field, String message)
+    {
+        this.field = field;
+    }
+    public DuplicateException(String messsage)
+    {
+        super(messsage);
     }
 }
--- src/main/java/ie/atu/cicdweek5/errorHandling/ExceptionDetails.java
+++ src/main/java/ie/atu/cicdweek5/errorHandling/ExceptionDetails.java
@@ -1,4 +1,13 @@
 package ie.atu.cicdweek5.errorHandling;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ExceptionDetails {
+    private String fieldName;
+    private String fieldValue;
 }
